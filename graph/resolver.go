55a38360426730559dc0e4ce22c97f5bbcdf7c24
Ayrton Souza <ayrtonsouza@icloud.com>
2021-10-14 17:20:20 -0300

    [:construction:] generating automatic skeleton

---
+++ graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/ayrtonbsouza/go-aluno-graphql/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	Categories []*model.Category
+}
